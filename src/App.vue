8418e87db7ccc73d377d1aea1d928ad8c4f24370
danielecarpentiero <carpentierodaniele@gmail.com>
2024-02-20 17:12:40 +0100

    feat: style.css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      cocktails: [],
+      baseUrl: 'http://127.0.0.1:8000/',
+      apiUrls: {
+        cocktails: 'api/cocktails'
+      },
+    }
+  },
+  methods: {
+    getCocktails() {
+      axios.get(this.baseUrl + this.apiUrls.cocktails).then(response => {
+        this.cocktails = response.data.results;
+        console.log(response)
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  created() {
+    this.getCocktails();
+  },
+  components: {
+
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div class="row mt-5">
+  <div class="card col col-md-6 g-5" v-for="cocktail in cocktails">
+    <div class="card-body">
+      <h5 class="card-title">{{ cocktail.nome_cocktail }}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">{{ cocktail.categoria_cocktail }}</h6>
+      <p class="card-text">Ingrediente principale: {{ cocktail.ingrediente_principale }}</p>
+      <p class="card-text">decorazione: {{ cocktail.decorazione }}</p>
+      <span>{{ cocktail.alcolico === 1 ? "si, è alcolico" : "no, non è alcolico" }}</span>
     </div>
-  </header>
+  </div>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style scoped lang="scss">
+  .card{
+    width: calc((100% / 2) - 20px);
+    margin: 10px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
